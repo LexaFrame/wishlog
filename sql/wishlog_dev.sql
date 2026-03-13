3b3e2d4f119b9adcb6110bdbcbcfafe91b7811e0
LexaFrame <sarah.bilger@free.fr>
2026-03-13 02:13:25 +0100

    Modification finale pour les insert de la base de données

--- sql/wishlog_dev.sql
+++ sql/wishlog_dev.sql
@@ -301,9 +301,25 @@ INSERT INTO `wl_category`(`category_name`, `wishlist_id`)
 VALUES 
 ('Jewelry', 1),
 ('Books', 2),
-('Vinyl', 2);
+('Clothes', 2);
 
-INSERT INTO `wl_product`
+INSERT INTO `wl_product`(`product_url`, `shop_name`, `product_name`, `product_image_url`, `product_description`, `product_price`, `product_origin`, `category_id`)
+VALUES 
+('https://www.lego.com/fr-fr/product/dinosaur-fossils-tyrannosaurus-rex-76968?CMP=AFC-AffiliateFR-TnL5HPStwNw-2116208-1706080-10', 'Lego', 'LEGO Tyrannosaurus Rex', 'https://www.lego.com/cdn/cs/set/assets/blt490fcaeb0097ae2f/76968_Prod.png?format=webply&fit=bounds&quality=75&width=800&height=800&dpr=1', 'Partagez votre passion pour les dinosaures avec un modèle impressionnant à exposer avec fierté : Les fossiles de dinosaures : le Tyrannosaurus rex (76968). Ce modèle LEGO® Jurassic World contient un fossile de T. rex à l\’échelle 1/12 doté d\’un crâne, de mâchoires, de bras et d\’une queue mobiles. Une plaque descriptive peut être fixée sur le support, et le site de fouilles du modèle inclut des surprises, dont un morceau « d\’ambre » et 2 minifigurines du film Jurassic Park de 1993 : Ellie Sattler et Alan Grant.', 249.99, 'manual', NULL),
+('https://www.amazon.fr/Au-coeur-lesprit-critique-manipulations/dp/2416009052/ref=sr_1_1?__mk_fr_FR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=K079NVJUR114&dib=eyJ2IjoiMSJ9.-JR84T4-ig3vqNONb7SJ38_iGpA4FN_m-IIqgvoBAfuejWu7Y1D83W0czENvWSrOq7JXPvNdL93pAAkwTZ5ar0VwsbMWdIBn7myuAVQhHyn16ExnqRrMZC0k6_PPlFxKlVMjanwGnR51XOlUOM326bK0VQqOy2tFPXVbgNPY-m3440-YiHhfzkWVesYV-zG2whoW8D1jDtjl_99jU0sqBA.94E6qaJv1g4__8hhg4Rt2LFLwdAZhXgjfSXaJerii78&dib_tag=se&keywords=au+coeur+de+l%27esprit+critique&qid=1765873127&sprefix=au+coeur+de+l%27esprit+critique%2Caps%2C94&sr=8-1','Amazon','Au coeur de l\'esprit critique','https://m.media-amazon.com/images/I/610das5U2NL._SL1400_.jpg','Comment démêler le vrai du faux, face au flux constant d\'informations auquel nous sommes exposés ? Fake news, théories du complot, désinformation, conseils bien intentionnés du voisin : de fausses croyances s\'immiscent dans nos jugements. Quelles sont les bonnes pratiques pour trier au mieux les informations ? Pourquoi faut-il se méfier de soi-même ? Comment lutter contre les fausses croyances ? Qu\'est-ce que l`\'esprit critique et quelle est son utilité ? Qui sont les zététiciens, ces vulgarisateurs sceptiques ? Quels éclairages apporte la psychologie ? L\'objectif de ce livre est d\'offrir des ressources, des outils et des méthodes à tous ceux qui souhaitent développer leur esprit critique, afin de déjouer les pièges et manipulations (rhétorique fallacieuse, biais cognitifs, etc.), pour des prises de décision plus éclairées et plus libres.',18.00,'manual', NULL),
+('https://odette-et-lulu.com/shop/foulard-balte/','Odette & Lulu','Foulard Balte Bleu Klein','https://odette-et-lulu.com/wp-content/uploads/2023/08/foulard-balte_bleu-klein_odette-et-lulu_05.jpg','Foulard en acrylique, taille 125cm x 125cm',42.00, 'manual', NULL);
+
+INSERT INTO `wluser_wlwishlist` (`user_id`,`wishlist_id`,`role_in_wishlist`)
+VALUES 
+(1,1, 'owner'),
+(2,2, 'owner'),
+(3,3, 'owner');
+
+INSERT INTO `wlwishlist_wlproduct` (`wishlist_id`, `product_id`, `product_quantity`,`buy_decision`, `purchase_cancelled`)
+VALUES
+(1, 3, 1, FALSE, FALSE),
+(2, 1, 1, FALSE, FALSE),
+(3, 2, 1, FALSE, FALSE);
 
 COMMIT;
 
